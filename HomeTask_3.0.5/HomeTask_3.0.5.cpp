4d425a09a9c9d6f02f3abb5ee3a6c2f331bfaded
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-20 23:14:15 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_3.0.5/HomeTask_3.0.5.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char arr[10] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+    int arr1[10] = { 0,1,2,3,4,5,6,7,8,9 };
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << arr[i] << arr1[j] << " ";
+        }
+        cout << endl;
+    }
+
+
+}
